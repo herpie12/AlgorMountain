9c4f448f9cd2928e0d9aebaf622bbec12cc46a0b
Herprit <herpritsingh12@gmail.com>
2020-07-25 12:50:20 +0200

    Algo init
    Testing methods, init

---
+++ MethodTesting/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using MethodsAL;
+using Xunit;
+
+namespace MethodTesting
+{
+    public class UnitTestMethods
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 3, 5, 7, 9 }, 3, 1)]
+        [InlineData(new int[] { 1, 3, 5, 7, 9 }, 5, 2)]
+        public void LinearSearchTestPass(int[] array, int item, int expectedPosition)
+        {
+            var foundposition = SearchMethods.LinearSearchFindPosition(array, item);
+
+            Assert.Equal(expectedPosition, foundposition);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 3, 5, 7, 9 }, 3, 4)]
+        [InlineData(new int[] { 1, 3, 5, 7, 9 }, 5, 1)]
+        public void LinearSearchTestFail(int[] array, int item, int expectedPosition)
+        {
+            var foundposition = SearchMethods.LinearSearchFindPosition(array, item);
+
+            Assert.NotEqual(expectedPosition, foundposition);
+        }
+    }
+}
---
+++ MethodsAL/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MethodsAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayTest = new int[] { 1, 3, 5, 7, 9 };
+
+            int result = SearchMethods.LinearSearchFindPosition(arrayTest, 9);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class SearchMethods
+    {
+
+        public static int LinearSearchFindPosition(int[] array, int item)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                if (array[i] == item)
+                {
+                    return i;
+                }
+
+            }
+
+            return 0;
+
+        }
+    }
+}
