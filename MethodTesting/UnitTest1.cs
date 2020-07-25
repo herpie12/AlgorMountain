b97cc2febcdd2f398932c29df6f6cd8dda746496
Herprit <herpritsingh12@gmail.com>
2020-07-25 14:13:52 +0200

    fixed naming, added binary search

--- MethodTesting/UnitTest1.cs
+++ MethodTesting/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace MethodTesting
         [InlineData(new int[] { 1, 3, 5, 7, 9 }, 5, 2)]
         public void LinearSearchTestPass(int[] array, int item, int expectedPosition)
         {
-            var foundposition = SearchMethods.LinearSearchFindPosition(array, item);
+            var foundposition = SearchMethods.LinearSearchFindIndex(array, item);
 
             Assert.Equal(expectedPosition, foundposition);
         }
@@ -21,9 +21,19 @@ namespace MethodTesting
         [InlineData(new int[] { 1, 3, 5, 7, 9 }, 5, 1)]
         public void LinearSearchTestFail(int[] array, int item, int expectedPosition)
         {
-            var foundposition = SearchMethods.LinearSearchFindPosition(array, item);
+            var foundposition = SearchMethods.LinearSearchFindIndex(array, item);
 
             Assert.NotEqual(expectedPosition, foundposition);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 3, 5, 7, 9 }, 3, 1)]
+        [InlineData(new int[] { 1, 3, 5, 7, 9 }, 5, 2)]
+        public void BinarySearchTest(int[] array, int item, int expectedPosition)
+        {
+            var foundposition = SearchMethods.BinarySearchFindIndex(array, item);
+
+            Assert.Equal(expectedPosition, foundposition);
         }
     }
 }
---
+++ MethodsAL/Searchmethods1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace MethodsAL
+{
+    public static class SearchMethods
+    {
+        
+            public static int LinearSearchFindIndex(int[] array, int item)
+            {
+
+                for (int i = 0; i < array.Length; i++)
+                {
+
+                    if (array[i] == item)
+                    {
+                        return i;
+                    }
+                }
+
+                return 0;
+
+            }
+
+        public static int BinarySearchFindIndex(int[] array, int item)
+        {
+
+            int left = 0;
+            int right = array.Length -1;
+
+            while (left<= right)
+            {
+                var middle = (left + right) / 2;
+
+                if (array[middle] == item)
+                        return middle;
+
+                if (item < array[middle])
+                {
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+
+                }
+            }
+
+            return -1;
+        }
+    }
+}
